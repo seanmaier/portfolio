1b1d2687f2400766efd4248889a00de98ed9e9d9
seanmaier <sean.maier@outlook.de>
2025-06-12 18:33:59 +0200

    Changed look of Hero; Made Hero and AboutMe fullly responsive

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,22 +2,29 @@ import Button from "./button/Button";
 import resumePicture from "../assets/IMG_90951.png";
 import { IoMailOutline } from "react-icons/io5";
 import { useTranslation } from "react-i18next";
+import { CiLinkedin } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa";
 
 const AboutMe = () => {
   const [t] = useTranslation("global", { keyPrefix: "about" });
 
   return (
-    <div className="bg-creme w-screen items-center justify-center p-15 lg:flex-col">
-      <div className="mb-8 flex flex-col gap-4">
+    <div className="bg-creme flex w-screen flex-col items-center justify-center gap-10 p-5 py-30 md:justify-evenly md:px-30 lg:flex-row">
+      <div className="flex max-w-lg flex-col gap-8 p-5">
         <h2
           id="resume"
-          className="text-[clamp(2.5rem,5vw,4rem)] font-bold italic"
+          className="text-[clamp(2.5rem,7vw,6rem)] font-bold italic"
         >
           {t("title")}
         </h2>
-        <p>{t("description")}</p>
+        <p className="text-[clamp(10px,20px,40px)] md:max-w-md">
+          {t("description")}
+        </p>
         <Button
           variant="tertiary"
+          size="lg"
+          iconPosition="left"
+          icon={<CiLinkedin className="" />}
           onClick={() =>
             (window.location.href = "https://www.linkedin.com/in/maiersean/")
           }
@@ -25,7 +32,8 @@ const AboutMe = () => {
           linkedin.com/in/maiersean
         </Button>
       </div>
-      <div className="max-w-sm lg:w-1/2">
+
+      <div className="max-w-sm md:max-w-md xl:max-w-lg">
         <div className="relative">
           <img
             src={resumePicture}
@@ -35,19 +43,34 @@ const AboutMe = () => {
           <span className="before:bg-primary-500 absolute inset-0 before:absolute before:inset-0 before:top-[35%] before:content-['']"></span>
           <span className="before:bg-creme absolute inset-0 before:absolute before:inset-[25%] before:top-[40%] before:h-[60%] before:w-[60%] before:rounded-full before:content-['']"></span>
         </div>
-        <div className="bg-black p-5">
-          <h6 className="font-jetbrains mb-3 text-3xl text-white italic">
+        <div className="flex flex-col gap-1 bg-black p-5">
+          <h6 className="font-jetbrains text-4xl text-white italic">
             {t("contact")}
           </h6>
-          <div
+          <Button
+            className="w-fit"
+            iconPosition="left"
+            variant="text"
+            icon={<FaGithub />}
+            onClick={() =>
+              window.open("https://github.com/seanmaier", "_blank")
+            }
+          >
+            <span className="font-jetbrains text-white">@seanmaier</span>
+          </Button>
+          <Button
+            className="w-fit"
+            iconPosition="left"
+            variant="text"
+            icon={<IoMailOutline />}
             onClick={() =>
               (window.location.href = "mailto:sean.maier@outlook.com")
             }
-            className="hover:bg-primary-500 flex cursor-pointer items-center gap-3 rounded-2xl p-2 transition-colors duration-300"
           >
-            <IoMailOutline className="text-2xl font-bold text-white" />
-            <p className="font-jetbrains text-white">sean.maier@outlook.de</p>
-          </div>
+            <span className="font-jetbrains text-white">
+              sean.maier@outlook.de
+            </span>
+          </Button>
         </div>
       </div>
     </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,60 +1,65 @@
 import { useTranslation } from "react-i18next";
 import me from "../assets/IMG_2285.jpeg";
+import { FaArrowDown } from "react-icons/fa";
 
 const Hero = () => {
   const [t] = useTranslation("global", { keyPrefix: "hero" });
 
   return (
-    <div id="about-me" className="bg-primary-500 w-screen flex-col">
-      <div className="align-center flex flex-col items-center justify-center p-8 pb-0 md:hidden">
-        <img
-          className="h-auto rounded-2xl text-white"
-          src={me}
-          alt={t("alt")}
-        />
-        <p className="font-birthstone h-20 -translate-y-40 text-8xl text-white">
-          {t("title")}
-        </p>
-        {Array.from({ length: 3 }).map((_, i) => (
-          <p
-            className="font-birthstone h-20 -translate-y-40 text-8xl text-transparent drop-shadow-sm [-webkit-text-stroke:1px_#f9b21d] [text-stroke:2px_#f9b21d]"
-            key={i}
-          >
-            {t("title")}
-          </p>
-        ))}
-      </div>
-
-      <div className="bg-primary-900 flex w-screen flex-col items-center justify-center gap-10 p-10 pb-0">
-        <p className="font-jetbrains absolute w-100 -translate-y-20 text-center text-white">
-          {t("description")}
-        </p>
-        <div className="bg-secondary-500 flex h-20 w-20 translate-y-10 animate-bounce items-center justify-center rounded-full">
-          <span className="text-xs font-bold">{t("scrollDown")}</span>
-        </div>
-      </div>
-
-      <div className="hidden">
-        <div className="flex flex-col items-center justify-center gap-10">
-          <img className="h-80 rounded-2xl" src="./src/assets/IMG_2285.JPEG" />
-          <p className="max-w-lg text-center text-white">
-            Focussed on learning new things and Exploring the world of software
-            architecture. Always in for a new challenge
-          </p>
-        </div>
-        <div>
-          <p className="font-birthstone text-8xl text-white">Portfolio</p>
-          {Array.from({ length: 3 }).map((_, i) => (
-            <p
-              className="font-birthstone text-primary-500 drop-shadow-secondary-500 text-8xl drop-shadow-sm"
-              key={i}
-            >
-              Portfolio
+    <>
+      <div id="about-me" className="bg-primary-500 w-screen flex-col pb-50">
+        <div className="flex flex-col items-center justify-center p-5 pb-0 md:flex-row md:gap-8 md:p-20 lg:justify-between">
+          <div className="flex flex-1 items-center justify-center md:basis-[60%] lg:basis-0">
+            <img
+              className="h-auto max-w-full rounded-2xl text-white lg:max-w-3xl"
+              src={me}
+              alt={t("Picture of me")}
+            />
+          </div>
+          <div className="relative flex flex-1 items-center justify-center md:gap-8">
+            <p className="font-birthstone absolute h-20 -translate-y-40 text-8xl text-white md:right-0 md:-translate-x-10 md:text-[150px] lg:-translate-y-70 lg:text-[200px]">
+              {t("title")}
             </p>
-          ))}
+            <p className="font-birthstone absolute h-20 -translate-y-20 text-8xl text-transparent drop-shadow-sm [-webkit-text-stroke:1px_#f9b21d] [text-stroke:2px_#f9b21d] md:right-0 md:-translate-x-10 md:-translate-y-15 md:text-[150px] lg:-translate-y-40 lg:text-[200px]">
+              {t("title")}
+            </p>
+            <p className="font-birthstone absolute h-20 -translate-y-0 text-8xl text-transparent drop-shadow-sm [-webkit-text-stroke:1px_#f9b21d] [text-stroke:2px_#f9b21d] md:right-0 md:-translate-x-10 md:translate-y-15 md:text-[150px] lg:-translate-y-10 lg:text-[200px]">
+              {t("title")}
+            </p>
+            <p className="font-birthstone absolute h-20 translate-y-20 text-8xl text-transparent drop-shadow-sm [-webkit-text-stroke:1px_#f9b21d] [text-stroke:2px_#f9b21d] md:right-0 md:-translate-x-10 md:translate-y-20 md:text-[150px] lg:translate-y-20 lg:text-[200px]">
+              {t("title")}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="bg-primary-900 relative flex h-90 items-center justify-center px-10">
+        <div className="relative hidden flex-1 items-center justify-center md:flex md:gap-8">
+          <p className="font-birthstone absolute h-20 -translate-y-40 text-8xl text-white md:right-0 md:translate-x-10 md:text-[150px] lg:-translate-y-50 lg:text-[220px]">
+            Resume
+          </p>
+          <p className="font-birthstone absolute h-20 -translate-y-20 text-8xl text-transparent drop-shadow-sm [-webkit-text-stroke:1px_#f9b21d] [text-stroke:2px_#f9b21d] md:right-0 md:translate-x-10 md:-translate-y-15 md:text-[150px] lg:-translate-y-20 lg:text-[220px]">
+            Resume
+          </p>
+          <p className="font-birthstone absolute h-20 -translate-y-20 text-8xl text-transparent drop-shadow-sm [-webkit-text-stroke:1px_#f9b21d] [text-stroke:2px_#f9b21d] md:right-0 md:translate-x-10 md:translate-y-10 md:text-[150px] lg:translate-y-10 lg:text-[220px]">
+            Resume
+          </p>
+          <p className="font-birthstone absolute h-20 translate-y-20 text-8xl text-transparent drop-shadow-sm [-webkit-text-stroke:1px_#f9b21d] [text-stroke:2px_#f9b21d] md:right-0 md:translate-x-10 md:translate-y-40 md:text-[150px] lg:translate-y-40 lg:text-[220px]">
+            Resume
+          </p>
+        </div>
+        <div className="flex justify-center md:basis-[70%]">
+          <p className="font-jetbrains max-w-lg text-2xl text-white">
+            {t("description")}
+          </p>
+        </div>
+        <div className="bg-secondary-500 absolute bottom-0 flex h-30 w-30 translate-y-20 animate-bounce flex-col items-center justify-center rounded-full">
+          <span className="flex flex-col text-xl font-bold">
+            {t("scrollDown")}
+          </span>
+          <FaArrowDown className="absolute bottom-3 text-3xl" />
+        </div>
+      </div>
+    </>
   );
 };
 
