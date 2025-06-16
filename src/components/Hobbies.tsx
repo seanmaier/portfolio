b035272890778c5b0a25c3acf583531699dd0a9d
seanmaier <sean.maier@outlook.de>
2025-06-17 00:52:37 +0200

    Added hobbies tab; Added Counter; Added ImageAccordian; Added ProgressBar

--- src/components/Hobbies.tsx
+++ src/components/Hobbies.tsx
@@ -1,28 +1,79 @@
-import { MediaModal } from "./MediaModal";
-import travelling from "../assets/IMG_2285.jpeg";
+import { useState } from "react";
+import cinematography from "../assets/IMG_1868.JPEG";
+import travelling from "../assets/IMG_3134.JPEG";
+import hiking from "../assets/IMG_2647.JPEG";
 import piano from "../assets/piano.jpg";
+import ImageAccordian, { type Item } from "./ImageAccordian";
+import ProgressBar from "./ProgessBar";
+
+const items: Item[] = [
+  {
+    title: "Travelling",
+    description:
+      "I love to explore new places and cultures, capturing memories along the way.",
+    url: travelling,
+  },
+  {
+    title: "Music",
+    description:
+      "I play the piano for fun and relaxation, to calm down after a long day.",
+    url: piano,
+  },
+  {
+    title: "Cinematography",
+    description:
+      "I enjoy creating videos and capturing moments through the lens of a camera.",
+    url: cinematography,
+  },
+  {
+    title: "Hiking",
+    description:
+      "Seeing the world from above is one of my favorite things to do.",
+    url: hiking,
+  },
+];
 
 const Hobbies = () => {
+  const [index, setIndex] = useState(2);
+
   return (
     <div
       id="hobbies"
-      className="bg-creme flex h-screen flex-col items-center justify-center gap-2"
+      className="bg-creme flex flex-col items-center justify-center gap-20 py-20 md:px-20 lg:px-40 xl:px-80 2xl:px-[30%]"
     >
-      <h2 className="text-[clamp(2.5rem,7vw,6rem)] font-bold italic">
-        Hobbies
-      </h2>
-      <div className="gap- flex w-full flex-col items-center justify-center p-6 md:flex-row md:gap-5">
-        <MediaModal
-          className="w-100"
-          imgSrc={travelling}
-          description="Das ist ein Test"
-        />
-        <MediaModal
-          className="w-100"
-          imgSrc={piano}
-          description="Das ist ein Test"
-        />
-        <MediaModal className="w-100" videoSrc={"../assets/IMG_2810.MP4"} />
+      <h2 className="text-center xl:text-8xl">More about me</h2>
+
+      <div
+        id="languages"
+        className="p flex w-full flex-col items-center justify-center gap-10"
+      >
+        <h3>The languages I speak:</h3>
+
+        <div className="flex flex-col items-center gap-10 md:flex-row">
+          <div className="flex flex-col items-center gap-0.5">
+            <p className="text-2xl font-bold">German</p>
+            <p className="font-semibold text-gray-500">Mouther tongue</p>
+
+            <ProgressBar progress={100} />
+          </div>
+
+          <div className="flex flex-col items-center gap-0.5">
+            <p className="text-2xl font-bold">English</p>
+            <p className="font-semibold text-gray-500">Fluent</p>
+            <ProgressBar progress={95} />
+          </div>
+
+          <div className="flex flex-col items-center gap-0.5">
+            <p className="text-2xl font-bold">Swedish</p>
+            <p className="font-semibold text-gray-500">Beginner</p>
+            <ProgressBar progress={10} />
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full px-5">
+        <h3 className="text-center">Hobbies</h3>
+        <ImageAccordian items={items} />
       </div>
     </div>
   );
---
+++ src/components/ImageAccordian.tsx
@@ -0,0 +1,51 @@
+import type { ItemType } from "./UnsplashGrid";
+
+// https://cruip.com/create-an-image-accordion-with-tailwind/
+
+interface Props {
+  items: Item[];
+}
+
+export type Item = {
+  title: string;
+  description: string;
+  url: string;
+};
+
+const ImageAccordian = ({ items }: Props) => {
+  return (
+    <>
+      <div className="group mt-3 mb-1 flex justify-center gap-2 max-lg:flex-col">
+        {items.map((item, i: number) => {
+          return (
+            <article
+              key={i}
+              className="group/article relative w-full overflow-hidden rounded-xl transition-all duration-600 ease-[cubic-bezier(.5,.85,.25,1.15)] before:absolute before:inset-x-0 before:bottom-0 before:h-2/3 before:bg-gradient-to-t before:from-black/90 before:transition-opacity after:absolute after:inset-0 after:rounded-lg after:bg-white/0 after:opacity-0 after:backdrop-blur after:transition-all focus-within:ring focus-within:ring-indigo-300 focus-within:before:opacity-100 lg:before:opacity-0 lg:hover:before:opacity-100 lg:group-focus-within:[&:not(:focus-within):not(:hover)]:w-[20%] lg:group-focus-within:[&:not(:focus-within):not(:hover)]:after:opacity-100 lg:group-hover:[&:not(:hover)]:w-[20%] lg:group-hover:[&:not(:hover)]:after:opacity-100"
+            >
+              <a
+                className="absolute inset-0 z-10 flex flex-col justify-end p-3 text-white"
+                href="#0"
+              >
+                <h1 className="font-jetbrains text-2xl font-medium transition duration-500 ease-[cubic-bezier(.5,.85,.25,1.8)] group-focus-within/article:translate-y-0 group-focus-within/article:opacity-100 group-focus-within/article:delay-500 group-hover/article:translate-y-0 group-hover/article:opacity-100 group-hover/article:delay-300 lg:translate-y-2 lg:opacity-0">
+                  {item.title}
+                </h1>
+                <span className="text-xl font-medium transition duration-500 ease-[cubic-bezier(.5,.85,.25,1.8)] group-focus-within/article:translate-y-0 group-focus-within/article:opacity-100 group-focus-within/article:delay-500 group-hover/article:translate-y-0 group-hover/article:opacity-100 group-hover/article:delay-500 lg:translate-y-2 lg:opacity-0">
+                  {item.description}
+                </span>
+              </a>
+              <img
+                className="h-90 w-full object-cover lg:h-[420px]"
+                src={item.url}
+                width="960"
+                height="480"
+                alt="Image 01"
+              />
+            </article>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default ImageAccordian;
